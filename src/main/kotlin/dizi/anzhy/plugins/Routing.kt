fb109c668a176951ea7b9ca54c7ea17f6e97592b
anzhy.diz <anzhy.nesty@gmail.com>
2022-07-13 22:38:24 -0400

    add database with images

--- src/main/kotlin/dizi/anzhy/plugins/Routing.kt
+++ src/main/kotlin/dizi/anzhy/plugins/Routing.kt
@@ -5,6 +5,7 @@ import dizi.anzhy.routes.root
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 
@@ -12,5 +13,9 @@ fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+
+        static("/images"){
+            resources("images")
+        }
     }
 }

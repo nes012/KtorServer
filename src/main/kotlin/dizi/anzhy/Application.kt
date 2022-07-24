4ab3a8563872ae80a9bc3c999b30a4dc75eab486
anzhy.diz <anzhy.nesty@gmail.com>
2022-07-24 18:00:16 -0400

    test root end point

--- src/main/kotlin/dizi/anzhy/Application.kt
+++ src/main/kotlin/dizi/anzhy/Application.kt
@@ -16,3 +16,4 @@ fun Application.module() {
     configureDefaultHeader()
     configureStatusPages()
 }
+
--- src/test/kotlin/dizi/anzhy/ApplicationTest.kt
+++ src/test/kotlin/dizi/anzhy/ApplicationTest.kt
@@ -5,17 +5,19 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
-import dizi.anzhy.plugins.*
 
 class ApplicationTest {
     @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
+    fun `access root endpoint, assert correct information`() = testApplication {
         client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Welcome to Anime API", bodyAsText())
+            assertEquals(
+                expected = HttpStatusCode.OK,
+                actual = status
+            )
+            assertEquals(
+                expected = "\"Welcome to Anime API\"",
+                actual = bodyAsText()
+            )
         }
     }
 }
